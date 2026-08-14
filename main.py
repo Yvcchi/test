862fdcadc778453b550099033d1c28888adfb2df
Yama <122332746+Yvcchi@users.noreply.github.com>
2026-08-14 23:39:26 +0800

    Update VNSTOCK_API_KEY fallback value

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 from datetime import datetime, timedelta
 from vnstock import Quote, Finance, Market, config
 
-VNSTOCK_KEY = os.getenv('VNSTOCK_API_KEY', 'vnstock_9a7c79177954459d6c1109f6ee8590e6')
+VNSTOCK_KEY = os.getenv('VNSTOCK_API_KEY', 'v6')
 try:
     config.set_api_key(VNSTOCK_KEY)
     print(f"✓ Đã cấu hình Vnstock API Key thành công!")
